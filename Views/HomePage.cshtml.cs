488ba3b094a4f34d07a2f68cb6b29fa3ff7adcaf
Daniel Nweze <Daniel.nweze@hotmail.com>
2025-11-30 18:09:16 +0100

    feat: add initial HomePage view using shared layout

---
+++ Views/HomePage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CMSPortfolio.Views
+{
+    public class HomePageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
