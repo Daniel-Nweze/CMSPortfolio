ab738e67910c50ca5410590567ed3222a33f8f48
Daniel Nweze <Daniel.nweze@hotmail.com>
2025-11-30 18:08:24 +0100

    feat: add base layout with header and footer

---
+++ Views/Partials/Layout/_Footer.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CMSPortfolio.Views.Partials.Layout
+{
+    public class _FooterModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
